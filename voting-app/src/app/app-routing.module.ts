a7e961cfbe3e527cf3432cfed4d862ddb63c7e84
Samuel S Ravella <samuelravella11@gmail.com>
2020-07-07 12:13:28 +0530

    Built Login page and basic dashboard

--- voting-app/src/app/app-routing.module.ts
+++ voting-app/src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	{
+		component: DashboardComponent,
+		path: 'dashboard'
+	},
+	{
+		component: HomeComponent,
+		path: 'home'
+	},
+	{
+		component: LoginComponent,
+		path: 'login'
+	}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [ RouterModule.forRoot(routes) ],
+	exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- voting-app/src/app/app.module.ts
+++ voting-app/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { PostcardComponent } from './postcard/postcard.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		DashboardComponent,
+		HomeComponent,
+		LoginComponent,
+		SearchComponent,
+		PostcardComponent
+	],
+	imports: [ BrowserModule, AppRoutingModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
